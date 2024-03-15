5db1ae97b8f26a1d035e0f8659472aefa1714582
Дарья Сендерихина <dawo.xorowo@gmail.com>
2024-03-15 22:27:08 +0200

    dep1

--- src/cloudinary/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary/cloudinary.provider.ts
@@ -1,15 +1,15 @@
 import { v2 as cloudinary } from 'cloudinary';
-import config from '../../config/keys';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 export const CloudinaryProvider = {
   provide: 'CLOUDINARY',
   useFactory: () => {
     return cloudinary.config({
-      cloud_name: config.CLOUDINARY_CLOUD_NAME,
-      api_key: config.CLOUDINARY_API_KEY,
-      api_secret:
-        config.CLOUDINARY_SECRET_KEY,
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET_KEY,
     });
   },
 };
-
